5b37f6f10c6d2a0369761d3b39566ff663eb6b50
sifat hassan <sifat27.sh22@gmail.com>
2020-04-12 18:26:32 +0600

    design all fragment and activity and modefi MainAcitc=vity class and HomeFragment Class

--- app/src/main/java/com/example/coronax/ui/country/CountryFragment.java
+++ app/src/main/java/com/example/coronax/ui/country/CountryFragment.java
@@ -1,4 +1,4 @@
-package com.example.coronax.ui.dashboard;
+package com.example.coronax.ui.country;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.coronax.R;
 
-public class DashboardFragment extends Fragment {
+public class CountryFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private CountryViewModel countryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        countryViewModel =
+                ViewModelProviders.of(this).get(CountryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_country, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        countryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/coronax/ui/country/CountryViewModel.java
+++ app/src/main/java/com/example/coronax/ui/country/CountryViewModel.java
@@ -1,16 +1,16 @@
-package com.example.coronax.ui.home;
+package com.example.coronax.ui.country;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class CountryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public CountryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
